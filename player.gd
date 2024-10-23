378f71a3fc0f0267fccd0fb57dfdc66e8a90e523
SpoopCats <five-envoy-charm@duck.com>
2024-10-22 20:33:07 -0400

    WaitBottom and MoveUp work
    Got some of it working where the state to wait at the bottom works and the state to move up works.

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var move_speed: int
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
--- wait_bottom.gd
+++ wait_bottom.gd
@@ -6,3 +6,12 @@ var btm_of_screen: Vector2 = Vector2(960, 1020) # bottom of screen + 20 to get t
 
 func Enter():
 	player.global_position = btm_of_screen
+	print("State = WaitBottom")
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed('ui_up'):
+		Transitioned.emit(self, 'moveup')
+
+
+func Exit():
+	pass
