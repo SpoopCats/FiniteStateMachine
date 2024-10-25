b4e739f3c3d38159b5f9baa3b16421ab55bc5cb3
SpoopCats <five-envoy-charm@duck.com>
2024-10-24 21:14:08 -0400

    More project cleanup
    Cleaned up more files and rearranged them. player.gd still exists as a reference to how I made this behaviour work before using a FSM.

--- scenes/player/temp_player.gd
+++ scenes/player/temp_player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var collision_checker: KinematicCollision2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,7 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func _physics_process(delta: float) -> void:
 	pass
