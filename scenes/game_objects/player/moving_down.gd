b0da945ac6b5a8443565c22fcfd1bcc2363fc082
SpoopCats <five-envoy-charm@duck.com>
2024-10-26 19:48:42 -0400

    Use group comparison to ID colliders
    Player now can differentiate what it collides with based upon global groups within the game. Ralph now can collider with any instantiated enemy and run the placeholder code, which will likely become "Game Over" code eventually.

--- scenes/game_objects/player/moving_down.gd
+++ scenes/game_objects/player/moving_down.gd
@@ -5,8 +5,7 @@ class_name MovingDown
 
 
 @onready var player: CharacterBody2D = $"../.."
-@onready var ememies = get_tree().get_first_node_in_group("enemy") as CharacterBody2D
-@onready var btm_pedestal = get_tree().get_first_node_in_group("btm_pedestal") as StaticBody2D
+
 
 var dir: Vector2 = Vector2.DOWN
 var speed: int = 470
@@ -26,14 +25,16 @@ func Update(delta: float):
 func Physics_Update(delta: float):
 	collision_info = player.move_and_collide(player_velocity * delta)
 	if collision_info:
-		if collision_info.get_collider() == btm_pedestal:
+		# store the node collided with using get_collider()
+		var node_collided_with = collision_info.get_collider()
+		# test it to see if its in certain groups
+		if node_collided_with.is_in_group('btm_pedestal'):
 			Transitioned.emit(self, "waitbtm")
-		# compares collider to first node in enemy group
-		if collision_info.get_collider() == ememies:
+		if node_collided_with.is_in_group('enemy'):
 			#placeholder code for something that happens when Ralph hits enemies
 			print("enemy!")
 			player.queue_free()
-			ememies.queue_free()
+			node_collided_with.queue_free()
 
 
 func Exit():
