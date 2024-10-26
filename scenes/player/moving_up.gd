227d5d19e87e73dafb71881787dc330afba89546
SpoopCats <five-envoy-charm@duck.com>
2024-10-25 20:06:09 -0400

    Basic enemy collision detection
    All layers are still 1 and mask 1, but I can now detect if Ralph hits an enemy. Placeholder code inserted that queuefree() Ralph and enemy he hits.

--- scenes/player/moving_up.gd
+++ scenes/player/moving_up.gd
@@ -2,11 +2,13 @@ extends State
 class_name MovingUp
 
 @onready var player: CharacterBody2D = $"../.."
+# provides a reference to enemy root node
+@onready var ememies = get_tree().get_first_node_in_group("enemy") as CharacterBody2D
+
 var dir: Vector2 = Vector2.UP
 var speed: int = 470
 var player_velocity = dir * speed
 var collision_info: KinematicCollision2D
-
 
 func Enter():
 	print("State = MovingUp")
@@ -16,13 +18,18 @@ func Enter():
 func Update(delta: float):
 	pass
 
-
 func Physics_Update(delta: float):
 	collision_info = player.move_and_collide(player_velocity * delta)
 	if collision_info:
 		if collision_info.get_collider().get('name') == 'TopPedestal':
 			print(collision_info.get_collider().get('name'))
 			Transitioned.emit(self, "waittop")
+		# compares collider to first node in enemy group
+		if collision_info.get_collider() == ememies:
+			#placeholder code for something that happens when Ralph hits enemies
+			print("enemy!")
+			player.queue_free()
+			ememies.queue_free()
 
 
 func Exit():
