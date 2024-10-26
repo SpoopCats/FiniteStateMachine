e25b5f90ad224466ec04efe20ed966d7ec14d2ad
SpoopCats <five-envoy-charm@duck.com>
2024-10-26 16:48:38 -0400

    Rejigged pedestals and score
    Pedestals are now separated scenes and score is increases every pass of the screen now. It's just more fun to see the number go up faster.

--- scenes/game_objects/player/moving_down.gd
+++ scenes/game_objects/player/moving_down.gd
@@ -1,10 +1,12 @@
 extends State
+
+
 class_name MovingDown
 
 
 @onready var player: CharacterBody2D = $"../.."
-# provides a reference to enemy root node
 @onready var ememies = get_tree().get_first_node_in_group("enemy") as CharacterBody2D
+@onready var btm_pedestal = get_tree().get_first_node_in_group("btm_pedestal") as StaticBody2D
 
 var dir: Vector2 = Vector2.DOWN
 var speed: int = 470
@@ -24,8 +26,7 @@ func Update(delta: float):
 func Physics_Update(delta: float):
 	collision_info = player.move_and_collide(player_velocity * delta)
 	if collision_info:
-		if collision_info.get_collider().get('name') == 'BtmPedestal':
-			print(collision_info.get_collider().get('name'))
+		if collision_info.get_collider() == btm_pedestal:
 			Transitioned.emit(self, "waitbtm")
 		# compares collider to first node in enemy group
 		if collision_info.get_collider() == ememies:
--- scenes/game_objects/player/moving_up.gd
+++ scenes/game_objects/player/moving_up.gd
@@ -1,9 +1,12 @@
 extends State
+
+
 class_name MovingUp
 
+
 @onready var player: CharacterBody2D = $"../.."
-# provides a reference to enemy root node
 @onready var ememies = get_tree().get_first_node_in_group("enemy") as CharacterBody2D
+@onready var top_pedestal = get_tree().get_first_node_in_group("top_pedestal") as StaticBody2D
 
 var dir: Vector2 = Vector2.UP
 var speed: int = 470
@@ -23,8 +26,7 @@ func Update(delta: float):
 func Physics_Update(delta: float):
 	collision_info = player.move_and_collide(player_velocity * delta)
 	if collision_info:
-		if collision_info.get_collider().get('name') == 'TopPedestal':
-			print(collision_info.get_collider().get('name'))
+		if collision_info.get_collider() == top_pedestal:
 			Transitioned.emit(self, "waittop")
 		# compares collider to first node in enemy group
 		if collision_info.get_collider() == ememies:
@@ -35,4 +37,5 @@ func Physics_Update(delta: float):
 
 
 func Exit():
+	GameEvents.emit_increase_score()
 	pass
