2f92543eb5543f4ea3c412becc7da77d30fa26e0
SpoopCats <five-envoy-charm@duck.com>
2024-11-02 09:52:24 -0400

    Basic main menu created
    Basic main menu with play and quit buttons hooked up. This is still not the main scene that launches if we export the .exe, so I need to change that later.

---
+++ scenes/UI/start_menu/start_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+# neighbours not set for buttons yet
+# see this tutorial to setup: https://youtu.be/Mx3iyz8AUAE?si=6mYd4O2LqKQbzZlQ&t=165
+
+func _ready() -> void:
+	$MarginContainer/VBoxContainer/Play.grab_focus()
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/main.tscn')
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
