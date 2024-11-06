edef1e3cc8d3848383a62f4f413793ad57635381
SpoopCats <five-envoy-charm@duck.com>
2024-11-05 19:07:52 -0500

    Change enemy scale and textures
    Enemy scale flipped for cats moving left. Also changed randomization of cat textures. All cat textures match the color palette at the moment.

--- scenes/game_objects/enemy/enemy.gd
+++ scenes/game_objects/enemy/enemy.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@export var cat_texture: Array[Texture] = []
+
 @export var dir = Vector2.RIGHT
 @export var speed = 100
 @export var lane_to_reopen: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	sprite_2d.texture = cat_texture.pick_random()
 	velocity = dir * speed
+	# flip the entire node to face the other way by altering it's scale
+	if velocity.x < 0:
+		scale = Vector2(-1, 1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
