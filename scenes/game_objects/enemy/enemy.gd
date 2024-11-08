9df488e3d1dcaf2093ecbc10ae012cfa4df83914
SpoopCats <five-envoy-charm@duck.com>
2024-11-08 08:36:11 -0500

    Code cleanup 2

--- scenes/game_objects/enemy/enemy.gd
+++ scenes/game_objects/enemy/enemy.gd
@@ -1,22 +1,24 @@
 extends CharacterBody2D
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
-@export var cat_texture: Array[Texture] = []
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+@export var cat_texture: Array[Texture] = []
 @export var dir = Vector2.RIGHT
 @export var speed = 100
 @export var lane_to_reopen: Vector2
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	sprite_2d.texture = cat_texture.pick_random()
 	velocity = dir * speed
-	# flip the entire node to face the other way by altering it's scale
+	# if travelling to the left
 	if velocity.x < 0:
+		# flip the entire node to face the other way by altering it's scale
 		scale = Vector2(-1, 1)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	_is_enemy_on_screen()
 
@@ -25,6 +27,7 @@ func _physics_process(delta: float) -> void:
 	move_and_collide(velocity * delta)
 
 
+# for releasing enemies from memory when off screen
 func _is_enemy_on_screen():
 	# 656 is the x position where a 32x32 sprite will be fully off screen on the right
 	# -16 is the x position ... off screen on the left
--- scenes/game_objects/food/food.gd
+++ scenes/game_objects/food/food.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
-@export var food_textures: Array[Texture] = []
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
-# Called when the node enters the scene tree for the first time.
+@export var food_textures: Array[Texture] = []
+
+
 func _ready() -> void:
 	sprite_2d.texture = food_textures.pick_random()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
