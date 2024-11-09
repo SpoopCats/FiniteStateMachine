17cea4df91b2b7cf8198ad8895815c5f452f22b0
SpoopCats <five-envoy-charm@duck.com>
2024-11-09 18:18:34 -0500

    Fix bug where high score doesn't update
    Need to load the score again on the game over screen after it is recorded by the player to make sure ScoreManager.high_score is up to date.

--- scenes/UI/game_over_menu/high_score_label.gd
+++ scenes/UI/game_over_menu/high_score_label.gd
@@ -3,4 +3,6 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# make sure ScoreManager.high_score is up to date
+	ScoreManager.load_score()
 	text = 'High Score: ' + str(ScoreManager.high_score)
