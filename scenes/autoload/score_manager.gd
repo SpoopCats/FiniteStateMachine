61047468ab7aa33e6c5f04cd8fbc0d9b6bb6ca83
SpoopCats <five-envoy-charm@duck.com>
2024-11-09 17:49:55 -0500

    Outline for save/load score added
    Functions defined, but no hook up to other aspects of the game on this commit.

--- scenes/autoload/score_manager.gd
+++ scenes/autoload/score_manager.gd
@@ -1,18 +1,28 @@
 extends Node
 
-
+const SCORESAVEFILE = "user://scoresave.txt"
 var current_score: int = 0
 var high_score: int = 0
 
 
 func _ready():
-	pass
-	#make sure high score is current from file
+	# make sure high score is up to date from savescorefile.txt file
+	load_score()
 
 
 func save_score():
-	pass
+	# open scoresave.txt with read and write access
+	var file = FileAccess.open(SCORESAVEFILE, FileAccess.WRITE_READ)
+	# store an int up to 4,294,967,296
+	file.store_32(high_score)
+	# release use of the file in order to avoid "file is in use" errors
+	file = null
 
 
 func load_score():
-	pass
+	# open scoresave.txt with read access
+	var file = FileAccess.open(SCORESAVEFILE, FileAccess.READ)
+	# ensure the file does exist
+	if FileAccess.file_exists(SCORESAVEFILE):
+		high_score = file.get_32()
+		print(high_score)
