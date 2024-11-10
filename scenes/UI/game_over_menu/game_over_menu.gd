bd2e84c3bed6fbaba827ac34b59065eb7144f744
SpoopCats <five-envoy-charm@duck.com>
2024-11-09 19:31:24 -0500

    High score notifier label added
    Label added to gameover screen that will appear when a new high score is achieved.

--- scenes/UI/game_over_menu/game_over_menu.gd
+++ scenes/UI/game_over_menu/game_over_menu.gd
@@ -2,10 +2,14 @@ extends Control
 
 # neighbours not set for buttons yet
 # see this tutorial to setup: https://youtu.be/Mx3iyz8AUAE?si=6mYd4O2LqKQbzZlQ&t=165
-
+@export var new_high_score_scene : PackedScene
+@onready var new_high_score_label: Control = $NewHighScore
 
 func _ready() -> void:
 	$ButtonContainer/VBoxContainer/MarginContainer3/PlayAgain.grab_focus()
+	if ScoreManager.current_score == ScoreManager.high_score:
+		# do something to notify player of new high score
+		new_high_score_label.show()
 
 
 func _on_play_again_pressed() -> void:
