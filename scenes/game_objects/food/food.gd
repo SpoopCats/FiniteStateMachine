f69d4f0f73487903d25f6566be6c9e78738140c3
SpoopCats <five-envoy-charm@duck.com>
2024-11-19 20:32:43 -0500

    rough food spawn tween
    Added a rough food spawn tween. Increases in scale to 1x, moves to the position, and spins 360 degrees.

--- scenes/game_objects/food/food.gd
+++ scenes/game_objects/food/food.gd
@@ -1,15 +1,43 @@
 extends Node2D
 
-
+@onready var food: Node2D = $"."
 @onready var sprite_2d: Sprite2D = $Sprite2D
-
-
 @export var food_textures: Array[Texture] = []
 
 
-func _ready() -> void:
-	sprite_2d.texture = food_textures.pick_random()
+# spot where the food spawns in
+# this is hardcoded and taken from the garbage can and garbage bag pos
+var top_spawn_pos = Vector2(274, 28)
+var btm_spawn_pos = Vector2(356, 319)
 
+# final position for tweens to end on, corresponds to the pedestals in the
+# main.tscn scene
+# these can likely be hooked up via code but are hard coded here now
+var final_top_food_pos = Vector2(320, 8)
+var final_btm_food_pos = Vector2(320, 352)
+
+
+func _ready() -> void:
+	scale = Vector2(0.5, 0.5)
+	# move to infront of the background, but behind garbage can / bag
+	get_parent().move_child(food, 4)
+	sprite_2d.texture = food_textures.pick_random()
+	# if behind the garbage CAN at the top of the scene
+	# do this tween
+	if position == top_spawn_pos:
+		var tween = create_tween()
+		tween.set_parallel(true)
+		tween.tween_property($".", 'scale', Vector2(1, 1), 0.35)
+		tween.tween_property($".", 'rotation', 6.283185, 0.35)
+		tween.tween_property($".", "position", final_top_food_pos, 0.35)
+	# if behind the garbage BAG at the btm of the scene
+	# do this tween
+	if position == btm_spawn_pos:
+		var tween = create_tween()
+		tween.set_parallel(true)
+		tween.tween_property($".", 'scale', Vector2(1, 1), 0.35)
+		tween.tween_property($".", 'rotation', 6.283185, 0.35)
+		tween.tween_property($".", "position", final_btm_food_pos, 0.35)
 
 func _process(delta: float) -> void:
 	pass
--- scenes/managers/food_manager.gd
+++ scenes/managers/food_manager.gd
@@ -5,14 +5,14 @@ extends Node
 # references the Main node
 @onready var game: Node2D = $".."
 
-var top_food_pos = Vector2(320, 8)
-var btm_food_pos = Vector2(320, 352)
+# hard coded locations of the garbage can and the garbage bag 
+var top_spawn_pos = Vector2(274, 28)
+var btm_spawn_pos = Vector2(356, 319)
 
 
 func _ready() -> void:
 	GameEvents.spawn_food_top.connect(_on_spawn_food_top)
 	GameEvents.spawn_food_btm.connect(_on_spawn_foot_btm)
-
 
 func _on_spawn_food_top():
 	# Make Ralph eat the existing food
@@ -22,7 +22,7 @@ func _on_spawn_food_top():
 			each.queue_free()
 	# Spawn in new food
 	var food_to_spawn = food_scene.instantiate()
-	food_to_spawn.position = top_food_pos
+	food_to_spawn.position = top_spawn_pos
 	# must use call deferred here or the food won't spawn right as game loads
 	game.add_child.call_deferred(food_to_spawn)
 
@@ -33,6 +33,6 @@ func _on_spawn_foot_btm():
 		for each in food_to_despawn:
 			each.queue_free()
 	var food_to_spawn = food_scene.instantiate()
-	food_to_spawn.position = btm_food_pos
+	food_to_spawn.position = btm_spawn_pos
 	# don't have to use call_deffered here, just did it to see if it still works
 	game.add_child.call_deferred(food_to_spawn)
