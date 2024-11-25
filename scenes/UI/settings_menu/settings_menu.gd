b65122caec9d5b2bc3fefd10f9770176a38eac7b
SpoopCats <five-envoy-charm@duck.com>
2024-11-25 18:53:39 -0500

    Created delete high scores scene
    Made a popup scene that will confirm if the player wants to delete their highscores.

--- scenes/UI/settings_menu/settings_menu.gd
+++ scenes/UI/settings_menu/settings_menu.gd
@@ -22,3 +22,11 @@ func _on_mute_music_check_box_toggled(toggled_on: bool) -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/UI/start_menu/start_menu.tscn")
+
+
+func _on_del_high_score_button_pressed() -> void:
+	pass
+	# want to instantiate a menu here to grab focus
+	# menu then runs the code below and closes itself on yes.
+	#ScoreManager.current_score = 0
+	#ScoreManager.save_score()
